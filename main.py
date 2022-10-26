a5e5d85bb3d4ce40e9f38bf31008d07cd8f36874
Haluk Eren Ozlu <hlk.eren1907@gmail.com>
2022-10-26 15:18:33 +0300

    ilk python projesi olusturuldu.

---
+++ main.py
@@ -0,0 +1,45 @@
+x = 15
+y = 14
+z = 13
+if  y>x and z>x and z>y :
+  print("en büyük sayı:z")
+  print("en küçük sayı:x")
+
+elif y<x and z>x and z>y :
+  print ("en büyük sayı:z")
+  print ("en küçük sayı:y")
+
+elif y>x and y>z and z>x:
+  print ("en büyük sayı:y")
+  print ("en küçük sayı:x")
+
+elif y>x and y>z and x>z:
+  print ("en büyük sayı:y")
+  print ("en küçük sayı:z") 
+
+elif x>z and x>y and y>z:
+  print ("en büyük sayı:x")
+  print ("en küçük sayı:z")
+
+elif x>z and x>y and y<z:
+  print ("en büyük sayı:x")
+  print ("en küçük sayı:y")
+
+else:
+  print("en büyük veya en küçük yok")
+  
+
+  
+
+
+
+
+  
+
+
+  
+  
+
+
+  
+
