a74e4165361207ebe5a86611b14c744fcff47e1e
Haluk Eren Ozlu <hlk.eren1907@gmail.com>
2022-11-08 21:26:15 +0300

    Reformat yapıldı weak warnings aza düşürüldü

--- main.py
+++ main.py
@@ -1,45 +1,29 @@
 x = 15
 y = 14
-z = 13
-if  y>x and z>x and z>y :
-  print("en buyuk sayi:z")
-  print("en kucuk sayi:x")
+z = 16
+if y > x and z > x and z > y:
+    print("en buyuk sayi:z")
+    print("en kucuk sayi:x")
 
-elif y<x and z>x and z>y :
-  print ("en buyuk sayi:z")
-  print ("en kucuk sayi:y")
+elif y < x and z > x and z > y:
+    print("en buyuk sayi:z")
+    print("en kucuk sayi:y")
 
-elif y>x and y>z and z>x:
-  print ("en buyuk sayi:y")
-  print ("en kucuk sayi:x")
+elif y > x and y > z and z > x:
+    print("en buyuk sayi:y")
+    print("en kucuk sayi:x")
 
-elif y>x and y>z and x>z:
-  print ("en buyuk sayi:y")
-  print ("en kucuk sayi:z") 
+elif y > x and y > z and x > z:
+    print("en buyuk sayi:y")
+    print("en kucuk sayi:z")
 
-elif x>z and x>y and y>z:
-  print ("en buyuk sayi:x")
-  print ("en kucuk sayi:z")
+elif x > z and x > y and y > z:
+    print("en buyuk sayi:x")
+    print("en kucuk sayi:z")
 
-elif x>z and x>y and y<z:
-  print ("en buyuk sayi:x")
-  print ("en kucuk sayi:y")
+elif x > z and x > y and y < z:
+    print("en buyuk sayi:x")
+    print("en kucuk sayi:y")
 
 else:
-  print("en buyuk veya en kucuk yok")
-  
-
-  
-
-
-
-
-  
-
-
-  
-  
-
-
-  
-
+    print("en buyuk veya en kucuk yok")
